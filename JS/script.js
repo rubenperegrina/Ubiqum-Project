7ce77d17af660ee0c1798bd57a0938795d9ac968
rubenperegrina <peregrinaruben@gmail.com>
2017-02-23 10:39:38 +0100

    Login Done

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,23 @@
 /**
  * Created by rubenperegrina on 22/2/17.
  */
+
+/*Login*/
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+/*Authentication*/
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+});
+
+/*Sign Out*/
+firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+}, function(error) {
+    // An error happened.
+});
